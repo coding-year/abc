b5c4a2e935a185184f1745f969fd198b56147c3d
coding-year <107755523+coding-year@users.noreply.github.com>
2022-06-19 19:45:33 +0530

    Add files via upload

---
+++ a1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class a1
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a,b,c,d;
+System.out.println("enter the no. first");
+a=sc.nextInt();
+System.out.println("enter the no. second");
+b=sc.nextInt();
+c=a+b;
+d=a*b;
+System.out.println("sum of a and b is="+c);
+System.out.println("product of a and b is="+d);
+}
+}
